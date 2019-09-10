fd971fa755222f91d8a96fbd4fb663e74841b57f
LambdaC <studycwg@gmail.com>
2019-09-11 00:44:48 +0800

    rtarget done

--- target1/test.sh
+++ target1/test.sh
@@ -1,3 +1,5 @@
 ./hex2raw -i touch1.txt | ./ctarget -q
 ./hex2raw -i touch2.txt | ./ctarget -q
 ./hex2raw -i touch3.txt | ./ctarget -q
+./hex2raw -i phase4.txt | ./rtarget -q
+./hex2raw -i phase5.txt | ./rtarget -q
